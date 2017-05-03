065237ba4dd572ff51c1cf051e87b7a796ce684c
Richard Deloge <richarddeloge@gmail.com>
2017-05-04 01:01:13 +0200

    - Issue with Doctrine Connection Factory : Connection is not always automatically closed at end of ReactPHP Request.     Overload Doctrine configuration to replace the Doctrine Connection Factory to keep in a static context the connection     to share between requests.

---
+++ tests/Doctrine/ConnectionFactoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * ReactPHP Symfony Bridge.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/reactphp/symfony Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\Tests\ReactPHPBundle;
+
+use Doctrine\DBAL\Driver;
+use Teknoo\ReactPHPBundle\Doctrine\ConnectionFactory;
+
+/**
+ * Class ConnectionFactoryTest.
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/symfony-react Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ *
+ * @covers \Teknoo\ReactPHPBundle\Doctrine\ConnectionFactory
+ */
+class ConnectionFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function buildFactory()
+    {
+        return new ConnectionFactory([]);
+    }
+
+    public function testCreateConnection()
+    {
+        $factory = $this->buildFactory();
+
+        $this->getMockBuilder(Driver::class)->setMockClassName('ConnectionMock')->getMock();
+
+        $connection1 = $factory->createConnection(['driverClass' => 'ConnectionMock']);
+
+        $connection2 = $factory->createConnection(['driverClass' => 'ConnectionMock']);
+
+        self::assertSame($connection1, $connection2);
+    }
+}
